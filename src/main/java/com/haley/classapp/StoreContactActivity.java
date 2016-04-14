85d3645e344501177610c43c06f2bd1f96c2f1ea
Haley Bengtson <hnbvt24@vt.edu>
2016-04-14 13:21:41 -0400

    pushing updates on app

---
+++ src/main/java/com/haley/classapp/StoreContactActivity.java
@@ -0,0 +1,24 @@
+package com.haley.classapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StoreContactActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_contact);
+
+        String newString;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                newString = null;
+            } else {
+                newString = extras.getString("(555)555-1234");
+            }
+        } else {
+            newString = (String) savedInstanceState.getSerializable("(555)555-1234");
+        }
+    }
+}
