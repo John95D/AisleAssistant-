b16187d894d854d39849d402014d1fa56920b235
Greg Piper <gpiper14@gmail.com>
2016-05-01 18:10:40 -0400

    Fixed Google Maps Set start position to EATS Blacksburg

--- app/src/main/java/com/haley/classapp/MapsActivity.java
+++ app/src/main/java/com/haley/classapp/MapsActivity.java
@@ -2,8 +2,6 @@ package com.haley.classapp;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,9 +28,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng eats = new LatLng(37.235263,-80.419877);
+
+        mMap.addMarker(new MarkerOptions().position(eats).title("EATS Blacksburg"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eats, 17.0f));
     }
 }
 
