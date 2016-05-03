138d4f34e136cb9056f10a2cdb8a3fcc5144e4f2
Haley Bengtson <hnbvt24@vt.edu>
2016-05-03 16:56:12 -0400

    Added MyAccount screen and updated Login screens

--- app/src/main/java/com/haley/classapp/CreateAccountActivity.java
+++ app/src/main/java/com/haley/classapp/CreateAccountActivity.java
@@ -1,14 +1,12 @@
 package com.haley.classapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.haley.classapp.R;
-
-public class CreateAccountActivity extends AppCompatActivity {
+public class CreateAccountActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +19,7 @@ public class CreateAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), MyStoreActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MyAccountActivity.class);
                 startActivity(intent);
             }
         });
